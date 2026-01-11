11822cc4de181ab85309a375696619c76d29c3e9
left4sid <pushkarshaw69@gmail.com>
2026-01-11 11:57:03 +0530

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -2,13 +2,12 @@ import { auth } from "./firebase.js";
 import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
-/*
-  SIMPLE ROUTE GUARD
-  - Allows logged-in users
-  - Redirects guests to index.html
-*/
+let checked = false;
 
 export function requireAuth(redirectTo = "index.html") {
+  if (checked) return;
+  checked = true;
+
   onAuthStateChanged(auth, user => {
     if (!user) {
       window.location.replace(redirectTo);
